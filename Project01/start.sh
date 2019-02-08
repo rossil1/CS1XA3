6bf9e04d87fef9594b4adaae006cfa029c51a456
Luke Rossi <rossil1@mcmaster.ca>
2019-02-08 10:33:48 -0500

    began work on main script selection framework

---
+++ Project01/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ..
+
+echo -e 'hello and welcome to the menu! Possible actions include:'
+echo -e '1) File Type Count\t\t2)TODO\n\n'
+
+read -p "Enter the number of the action you'd like to preform:" SEL
+
+echo $SEL
