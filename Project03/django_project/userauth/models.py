f0e117e229902c415e091ae9db591d1654245e14
Luke Rossi <rossil1@mcmaster.ca>
2019-04-17 00:00:25 -0400

    sql table work for userauth app

--- Project03/django_project/userauth/models.py
+++ Project03/django_project/userauth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class User(models.Model):
+    uname = models.CharField(max_length=20, primary_key=True)
+    pw = models.CharField(max_length=20)
+
 # Create your models here.
