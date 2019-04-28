6d0d2cc65f4a696dae6a85aa32a7b849c7ab2fc9
Luke Rossi <rossil1@mcmaster.ca>
2019-04-27 21:24:39 -0400

    Login Page frontend finished

--- Project03/src/Login.elm
+++ Project03/src/Login.elm
@@ -1,4 +1,4 @@
-module Login exposing (main)
+module Main exposing (main)
 
 import Bootstrap.Button as Button
 import Bootstrap.CDN as CDN
@@ -10,11 +10,12 @@ import Bootstrap.Utilities.Spacing as Spacing
 import Browser
 import Browser.Navigation exposing (load)
 import Html exposing (Html, br, button, div, h1, input, p, text)
-import Html.Attributes exposing (value, align, autofocus, disabled, hidden, placeholder, style)
+import Html.Attributes exposing (align, autofocus, disabled, hidden, placeholder, style, type_, value)
 import Html.Events exposing (onClick, onInput)
-import Json.Encode as JEncode
-import Json.Decode as JDecode
 import Http
+import Json.Decode as JDecode
+import Json.Encode as JEncode
+import String
 
 rootUrl = "http://www.mac1xa3.ca/e/rossil1/Project03/"
 
@@ -26,63 +27,58 @@ type alias Model =
     , notif : String
     }
 
-encode : Model -> JEncode.value
-encode model = 
+encode : Model -> JEncode.Value
+encode model =
     JEncode.object
-    [ ("username", JEncode.string model.user)
-    , ("password", JEncode.string model.pass)
-    , ("login", JEncode.bool <| not(model.createUser) )
-    ]
-    
-    
+        [ ( "username", JEncode.string model.user )
+        , ( "password", JEncode.string model.pass )
+        , ( "login", JEncode.bool <| not model.createUser )
+        ]
 
-loginPost : Model -> Cmd Msg
-loginPost model = Http.post 
-    { url = rootUrl ++ "login/"
-    , body = Http.jsonBody <| encode model
-    , expect = Http.expectString Response
-    }
+jsonPost : Model -> Cmd Msg
+jsonPost model =
+    Http.post
+        { url = rootUrl ++ "login/"
+        , body = Http.jsonBody <| encode model
+        , expect = Http.expectString Response
+        }
 
 initialModel : () -> ( Model, Cmd Msg )
 initialModel _ =
     ( { user = "", pass = "", passChk = "", createUser = False, notif = "" }, Cmd.none )
 
-
 type Msg
     = DeltaUser String
     | DeltaPass String
     | DeltaPassChk String
-    | Login
     | CreateAcc
-    | NewUser
+    | Verify
     | Response (Result Http.Error String)
 
-
-accVerify : Model -> Model
+accVerify : Model -> (Model, Cmd Msg)
 accVerify model =
     case model.createUser of
         True ->
             if model.user == "" then
-                { model | notif = "Please enter a username" }
+                ({ model | notif = "Please enter a username" }, Cmd.none)
 
             else if model.pass == "" then
-                { model | notif = "Please enter a password" }
+                ({ model | notif = "Please enter a password" }, Cmd.none)
 
             else if model.pass /= model.passChk then
-                { model | notif = "Passwords do not match, take a check and try again." }
-
+                ({ model | notif = "Passwords do not match, take a check and try again." }, Cmd.none)
             else
-                { model | notif = "Passwords are equal -> Django for SQL Query on user for final check" }
+                ( model, jsonPost model )
 
         False ->
             if model.user == "" then
-                { model | notif = "Please enter a username" }
+                ({ model | notif = "Please enter a username" }, Cmd.none)
 
             else if model.pass == "" then
-                { model | notif = "Please enter a password" }
+                ({ model | notif = "Please enter a password" }, Cmd.none)
 
             else
-                { model | notif = "send JSON -> Django server for User.authenticate(user, pass)" }
+                ( model, jsonPost model )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -97,11 +93,8 @@ update msg model =
         DeltaPassChk newPassChk ->
             ( { model | passChk = newPassChk }, Cmd.none )
 
-        Login ->
-            ( accVerify model, Cmd.none )
-
-        NewUser ->
-            ( accVerify model, Cmd.none )
+        Verify ->
+            accVerify model
 
         CreateAcc ->
             ( { model
@@ -112,16 +105,21 @@ update msg model =
               }
             , Cmd.none
             )
-        Response result ->
-            case result of 
-                Ok "denied" ->
-                    ({model|notif = "Your access was denied. Check your info, or make an account."}, Cmd.none)
-                Ok "pass" ->
-                    (model, load ( rootUrl ++ "app/")
-                Err error ->
-                    (errorGet model error, Cmd.none)
-        
 
+        Response result ->
+            case result of
+                Ok "denied" ->
+                    ( { model | notif = "Your access was denied. Check your info, or make an account." }, Cmd.none )
+
+                Ok "usernameTaken" ->
+                    ({model|notif="That username is taken. Pick another"},Cmd.none)
+                Ok "accountCreated" ->
+                    ({model|notif="Account created! Now go back and login."}, Cmd.none)
+                Ok _ ->
+                    ( model, load (rootUrl ++ "app/") )
+
+                Err error ->
+                    ( errorGet model error, Cmd.none )
 
 view : Model -> Html Msg
 view model =
@@ -131,14 +129,14 @@ view model =
         , CDN.stylesheet
         , Grid.container [ hidden model.createUser ]
             [ Grid.row [ Row.centerMd ]
-                [ Grid.col [ Col.lg3 ] [ input  [ onInput DeltaUser, placeholder "Username", value model.user ] [ ] ]
+                [ Grid.col [ Col.lg3 ] [ input [ type_ "text", onInput DeltaUser, placeholder "Username", value model.user ] [] ]
                 , Grid.colBreak []
-                , Grid.col [ Col.lg3 ] [ input  [ onInput DeltaPass, placeholder "Password", Spacing.mt2, value model.pass  ] [ ] ]
+                , Grid.col [ Col.lg3 ] [ input [ type_ "password", onInput DeltaPass, placeholder "Password", Spacing.mt2, value model.pass ] [] ]
                 , Grid.colBreak []
                 ]
             , Grid.row [ Row.centerMd ]
                 [ Grid.col []
-                    [ button [ onClick Login, Spacing.mr2, Spacing.mt3, Keyboard.on Keyboard.Keydown [(Enter, Login) ] ] [ text "Login" ]
+                    [ button [ onClick Verify, Spacing.mr2, Spacing.mt3 ] [ text "Login" ]
                     , button [ onClick CreateAcc, Spacing.ml2, Spacing.mt3 ] [ text "Create Account" ]
                     ]
                 ]
@@ -147,16 +145,16 @@ view model =
         -- CREATE USER CONTAINER BELOW --
         , Grid.container [ hidden <| not model.createUser ]
             [ Grid.row [ Row.centerMd ]
-                [ Grid.col [ Col.lg3 ] [ input [ onInput DeltaUser, placeholder "Username", value model.user]  [  ] ]
+                [ Grid.col [ Col.lg3 ] [ input [ type_ "text", onInput DeltaUser, placeholder "Username", value model.user ] [] ]
                 , Grid.colBreak []
-                , Grid.col [ Col.lg3 ] [ input [ onInput DeltaPass, placeholder "Password", Spacing.mt2, value model.pass  ] [ ] ]
+                , Grid.col [ Col.lg3 ] [ input [ type_ "password", onInput DeltaPass, placeholder "Password", Spacing.mt2, value model.pass ] [] ]
                 , Grid.colBreak []
-                , Grid.col [ Col.lg3 ] [ input [ onInput DeltaPassChk, placeholder "Password Again", Spacing.mt2, value model.passChk ]  [  ] ]
+                , Grid.col [ Col.lg3 ] [ input [ type_ "password", onInput DeltaPassChk, placeholder "Password Again", Spacing.mt2, value model.passChk ] [] ]
                 ]
             , Grid.row [ Row.centerMd ]
                 [ Grid.col []
                     [ button [ onClick CreateAcc, Spacing.mr1, Spacing.mt3 ] [ text "Back" ]
-                    , button [ onClick NewUser, Spacing.ml1, Spacing.mt3 ] [ text "Submit" ]
+                    , button [ onClick Verify , Spacing.ml1, Spacing.mt3 ] [ text "Submit" ]
                     ]
                 ]
             ]
@@ -165,21 +163,22 @@ view model =
 
 errorGet : Model -> Http.Error -> Model
 errorGet model error =
-    case error of 
-        Http.BadUrl ulr ->
-            ({model| notif = "bad url: " ++ url}, Cmd.none)
-            
+    case error of
+        Http.BadUrl url ->
+            { model | notif = "bad url: " ++ url }
+
         Http.Timeout ->
-            ({model|notif = "Timeout!"}, Cmd.none)
-        
+            { model | notif = "Timeout!" }
+
         Http.NetworkError ->
-            ({model|notif = "Network Error!"}, Cmd.none)
+            { model | notif = "Network Error!" }
+
         Http.BadStatus status ->
-            ({model|notif = "Bad Status: " ++ status}, Cmd.none)
+            { model | notif = "Bad Status: " ++ String.fromInt status }
+
+        -- Http.BadStatus Int
         Http.BadBody resp ->
-            ({model|notif = "Bad Body: " ++ resp}, Cmd.none)
-        
-        
+            { model | notif = "Bad Body: " ++ resp }
 
 main : Program () Model Msg
 main =
